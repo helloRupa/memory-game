a029e7d2cd0107780754ea0a52208527659fd49d
Rupa Dhillon <rupa211984@gmail.com>
2018-12-13 17:15:12 -0800

    Add @guess_pair & turn and game playing logic

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,9 @@ class Game
   def initialize(size, player)
     @board = Board.new(size)
     @player = player.new(size)
+    @guess_pair = []
     welcome
+    play
   end
 
   def welcome_message
@@ -18,9 +20,48 @@ class Game
   end
 
   def welcome
-    system('clear')
+    # system('clear')
     welcome_message
     sleep(2)
+  end
+
+  def show_board
+    @board.render
+  end
+
+  def play
+    # system('clear')
+    until win?
+      show_board
+      turn
+    end
+  end
+
+  def turn
+    @guess_pair = []
+    process_guess
+    show_board
+    process_guess
+  end
+
+  def process_guess
+    guess = valid_guess
+    x, y = guess
+    @board[x, y].show
+    @guess_pair << guess
+  end
+
+  def valid_guess
+    x, y = @player.make_guess
+    while @board[x, y].face_up
+      puts 'Please choose a card that is face down'
+      x, y = @player.make_guess
+    end
+    [x, y]
+  end
+
+  def win?
+    @board.board_complete?
   end
 end
 
