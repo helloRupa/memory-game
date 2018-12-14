a5d0a27e78045de9db7509b3f60367243722ebca
Rupa Dhillon <rupa211984@gmail.com>
2018-12-14 01:04:05 -0800

    Break up matches_in_guesses into 2 more methods & shorten code elsewhere

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,8 +1,6 @@
 # First guess = random pair choice
 # Store guess results and make matches when found
 
-# require_relative './board.rb'
-
 class ComputerPlayer
   def initialize(size)
     @size = size
@@ -21,48 +19,52 @@ class ComputerPlayer
     guess = matches_in_guesses || valid_random_guess
     update_guess_arrays(guess)
     puts "Guess is #{guess[0]}, #{guess[1]}"
-    # puts "Guess pair is #{@guess_pair}"
-    puts "Guesses is #{@guesses}"
     pause
     guess
   end
 
   def update_guess_arrays(guess)
     @guess_pair << guess
-    if @guess_pair.length == 2
-      if @board.match?(@guess_pair)
-        @guesses.delete(@guess_pair[0])
-        @guesses.delete(@guess_pair[1])
-      else
-        @guesses << @guess_pair[0] unless @guesses.include?(@guess_pair[0])
-        @guesses << @guess_pair[1] unless @guesses.include?(@guess_pair[1])
-      end
-      @guess_pair = []
+    return nil unless @guess_pair.length == 2
+    if @board.match?(@guess_pair)
+      @guesses.delete(@guess_pair[0])
+      @guesses.delete(@guess_pair[1])
+    else
+      @guesses << @guess_pair[0] unless @guesses.include?(@guess_pair[0])
+      @guesses << @guess_pair[1] unless @guesses.include?(@guess_pair[1])
     end
+    @guess_pair = []
   end
 
   def matches_in_guesses
-    if @guess_pair.empty?
-      @guesses.each do |coords|
-        if @guesses.count(@board[*coords].value) > 1
-          @guesses.delete(coords)
-          return coords
-        end
+    find_match_in_guesses || find_match_in_guesses_and_guess_pair
+  end
+
+  def find_match_in_guesses_and_guess_pair
+    return nil if @guess_pair.empty?
+    @guesses.each do |coords|
+      next if coords == @guess_pair[0]
+      if @board.match?([coords, @guess_pair[0]])
+        @guesses.delete(coords)
+        return coords
       end
-    else
-      @guesses.each do |coords|
-        next if coords == @guess_pair[0]
-        if @board.match?([coords, @guess_pair[0]])
-          @guesses.delete(coords)
-          return coords
-        end
+    end
+    nil
+  end
+
+  def find_match_in_guesses
+    return nil unless @guess_pair.empty?
+    @guesses.each do |coords|
+      if @guesses.count(@board[*coords].value) > 1
+        @guesses.delete(coords)
+        return coords
       end
     end
     nil
   end
 
   def pause
-    sleep(5)
+    sleep(2)
   end
 
   def random_guess
@@ -71,9 +73,7 @@ class ComputerPlayer
 
   def valid_random_guess
     x, y = random_guess
-    while @board[x, y].face_up
-      x, y = random_guess
-    end
+    x, y = random_guess while @board[x, y].face_up
     [x, y]
   end
 end
